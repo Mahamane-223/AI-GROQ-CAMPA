9845af28a2e756c701d4ba9d351d7588335966c1
Maiga Mahamane Ahmad <mahamaneahmad1@gmail.com>
2026-06-29 15:21:11 +0200

    feat: added connection to groq

---
+++ main.py
@@ -0,0 +1,32 @@
+from groq import Groq
+
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("GROQ_API_KEY")
+
+client = Groq(api_key=API_KEY)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        
+        {
+            "role": "system",
+            "content": "tu es Son Goku de dragon ball"
+        },
+        
+        {
+            "role": "user",
+            "content": "tu connais luffy ?",
+        }
+    ],
+
+    
+    model="llama-3.3-70b-versatile"
+)
+
+# Print the completion returned by the LLM.
+print(chat_completion.choices[0].message.content)
